fb2d05b75be22c5a6329a5185d0ae658b57ad9b3
Rck27 <42706773+Rck27@users.noreply.github.com>
2023-02-28 06:11:20 +0700

    Create main.sh
    first commit

---
+++ main.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+
+read -p "your mikrotik local ip address,(ex: 10.10.x.1): " localip
+read -p "your proxmox ip address,(ex: 10.12.x.2): " pxmip
+read -p "your mikrotik on vlan ip address,(ex: 10.12.x.1): " mkip
+read -p "your name, (ex: kiki): " name
+read -p "dns name, (ex: smkhooh.net): " fqdn
+apt install php-fpm mariadb-server apache2 bind9 wget unzip -y
+
+IFS=. read ip1 ip2 ip3 ip4 <<< "$pxmip"
+
+mysql -u root --execute "CREATE DATABASE wordpress;
+CREATE USER 'wp_user'@'localhost' IDENTIFIED BY '12345';
+GRANT ALL ON wordpress.* TO 'wp_user'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+EXIT";
+
+cd /tmp
+wget https://wordpress.org/latest.zip
+unzip latest.zip
+sed -e "s,database_name_here,wordpress," -e "s,username_here,wp_user," -e "s,password_here,12345," wordpress/wp-config-sample.php > wordpress/wp-config.php
+mv wordpress /var/www/html/
+
+cd /etc/bind
+
+cp db.local fwd.$name;
+cp db.127 rev.$name;
+cat <<EOT>> named.conf.default-zones
+zone "$fqdn" {
+	type master;
+	file "/etc/bind/fwd.$name";
+};
+zone "$ip3.$ip2.$ip1.in-addr.arpa" {
+	type master;
+	file "/etc/bind/rev.$name";
+};
+
+EOT
+#echo -e "zone '$fqdn' { type master; file '/etc/bind/fwd.$name'; };" >> named.conf.default-zones
+#echo -e "zone '$ip3.$ip2.$ip1.in-addr.arpa' { type master; file '/etc/bind/rev.$name'; };" >> named.conf.default-zones
+
+echo -e "@ IN A $pxmip" >> fwd.$name
+echo -e "$ip4 IN PTR $fqdn." >> rev.$name
+
+echo -e "nameserver $pxmip \n search $fqdn" \n nameserver 8.8.8.8" > /etc/resolv.conf
+
+read -p "change apache2 document root to wordpress? Y/N: " ganti
+if [[ $ganti == "Y" ]]; then
+cd /etc/apache2/sites-available
+sed "s,/html,/html/wordpress," 000-default.conf -i
+fi
+
+
+systemctl restart bind9
+systemctl restart apache2
